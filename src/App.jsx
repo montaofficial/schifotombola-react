979b8d8b928a08d60a67aae2dec4c291fdc587e1
TK Mac Studio Editor <editor@tokiostudio.it>
2023-12-22 09:29:43 +0100

    release v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,63 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { current, extractNumber, last } from "./data";
 import Table from './Table';
 
 function App() {
+
+   const [table, setTable] = useState(getTable());
+   const [remaining, setRemaining] = useState(getRemaining());
+   const [last, setLast] = useState(getLast());
+   const [current,setCurrent] = useState(getCurrent());
+
+  useEffect(() => {
+ localStorage.setItem("table", JSON.stringify(table))
+},[table]);
+
+useEffect(() => {
+ localStorage.setItem("current", JSON.stringify(current))
+}, [current]);
+
+useEffect(() => {
+ localStorage.setItem("last", JSON.stringify(last))
+}, [last]);
+
+useEffect(() => {
+ localStorage.setItem("remaining", JSON.stringify(remaining))
+}, [remaining]);
+
+  const extractNumber = () => {
+    if (remaining.length === 0) {
+      throw new Error("No more numbers to extract");
+  }
+    const randomIndex = Math.floor(Math.random() * remaining.length);
+    const extractedNumber = remaining[randomIndex];
+  
+    // Remove the extracted number from the array
+    setRemaining(remaining.filter((number) => number !== extractedNumber));
+  
+    setCurrent(extractedNumber);
+    let temparray = [...last];
+    temparray.unshift(extractedNumber);
+    if (temparray.length > 5)temparray.pop();
+    setLast(temparray);
+    let temparray2 = [...table];
+    temparray2[extractedNumber] = true;
+    setTable(temparray2);
+  
+    return extractedNumber;
+  }
+
+  const clearAll = ()=>{
+    localStorage.clear();
+    setCurrent(getCurrent());
+    setLast(getLast());
+    setRemaining(getRemaining());
+    setTable(getTable());
+
+  }
   return (
     <>
-    <Table/>
+    <Table table={table}/>
       <div className="current">
         <h2>ESTRATTO IL</h2>
         <div className="current-number">{current}</div>
@@ -14,12 +66,47 @@ function App() {
       <div className="last">
         <h3>ULTIMI</h3>
         <div className="last-container">
-          {last.value.map(value => <div key={value} className="last-number">{value}</div>)}
+          {last.map(value => (<div key={value} className="last-number">{value}</div>))}
         </div>
       </div>
       <button onClick={extractNumber}>EXTRACT</button>
+      <button onClick={clearAll}>RESTART</button>
     </>
   );
 }
 
 export default App;
+
+function initTable() {
+  return Array.from({ length: 91 }, (_, i) => false);
+}
+
+
+// Function to create an array with numbers from 1 to 90
+function createNumberArray() {
+  return Array.from({ length: 90 }, (_, i) => i + 1);
+}
+
+function getRemaining() {
+  const value = localStorage.getItem("remaining");
+  if (!value) return createNumberArray();
+  return JSON.parse(value);
+}
+
+function getLast() {
+  const value = localStorage.getItem("last");
+  if (!value) return [];
+  return JSON.parse(value);
+}
+
+function getCurrent() {
+  const value = localStorage.getItem("current");
+  if (!value) return 0;
+  return JSON.parse(value);
+}
+
+function getTable() {
+  const value = localStorage.getItem("table");
+  if (!value) return initTable();
+  return JSON.parse(value);
+}
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,302 +1,295 @@
-import { useComputed } from "@preact/signals-react";
-import { table } from "./data";
 
-export default function Table() {
-  console.log(table);
-  const length = useComputed(() => table.value.length);
+export default function Table({table}) {
   return (
     <div className="grid">
       <div className="row">
-        <span className={"number " +(table.value[1] ? "extracted" : "")} id="1">
+        <span className={"number " +(table[1] ? "extracted" : "")} id="1">
           1
         </span>
-        <span className={"number " +(table.value[2] ? "extracted" : "")} id="2">
+        <span className={"number " +(table[2] ? "extracted" : "")} id="2">
           2
         </span>
-        <span className={"number " +(table.value[3] ? "extracted" : "")}id="3">
+        <span className={"number " +(table[3] ? "extracted" : "")}id="3">
           3
         </span>
-        <span className={"number " +(table.value[4] ? "extracted" : "")} id="4">
+        <span className={"number " +(table[4] ? "extracted" : "")} id="4">
           4
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="5">
+        <span className={"number " +(table[5] ? "extracted" : "")} id="5">
           5
         </span>
-        <span className={"number " +(table.value[6] ? "extracted" : "")} id="6">
+        <span className={"number " +(table[6] ? "extracted" : "")} id="6">
           6
         </span>
-        <span className={"number " +(table.value[7] ? "extracted" : "")} id="7">
+        <span className={"number " +(table[7] ? "extracted" : "")} id="7">
           7
         </span>
-        <span className={"number " +(table.value[8] ? "extracted" : "")} id="8">
+        <span className={"number " +(table[8] ? "extracted" : "")} id="8">
           8
         </span>
-        <span className={"number " +(table.value[9] ? "extracted" : "")} id="9">
+        <span className={"number " +(table[9] ? "extracted" : "")} id="9">
           9
         </span>
-        <span className={"number " +(table.value[10] ? "extracted" : "")} id="10">
+        <span className={"number " +(table[10] ? "extracted" : "")} id="10">
           10
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[11] ? "extracted" : "")} id="11">
+        <span className={"number " +(table[11] ? "extracted" : "")} id="11">
           11
         </span>
-        <span className={"number " +(table.value[12] ? "extracted" : "")} id="12">
+        <span className={"number " +(table[12] ? "extracted" : "")} id="12">
           12
         </span>
-        <span className={"number " +(table.value[13] ? "extracted" : "")} id="13">
+        <span className={"number " +(table[13] ? "extracted" : "")} id="13">
           13
         </span>
-        <span className={"number " +(table.value[14] ? "extracted" : "")} id="14">
+        <span className={"number " +(table[14] ? "extracted" : "")} id="14">
           14
         </span>
-        <span className={"number " +(table.value[15] ? "extracted" : "")} id="15">
+        <span className={"number " +(table[15] ? "extracted" : "")} id="15">
           15
         </span>
-        <span className={"number " +(table.value[16] ? "extracted" : "")} id="16">
+        <span className={"number " +(table[16] ? "extracted" : "")} id="16">
           16
         </span>
-        <span className={"number " +(table.value[17] ? "extracted" : "")} id="17">
+        <span className={"number " +(table[17] ? "extracted" : "")} id="17">
           17
         </span>
-        <span className={"number " +(table.value[18] ? "extracted" : "")} id="18">
+        <span className={"number " +(table[18] ? "extracted" : "")} id="18">
           18
         </span>
-        <span className={"number " +(table.value[19] ? "extracted" : "")} id="19">
+        <span className={"number " +(table[19] ? "extracted" : "")} id="19">
           19
         </span>
-        <span className={"number " +(table.value[20] ? "extracted" : "")} id="20">
+        <span className={"number " +(table[20] ? "extracted" : "")} id="20">
           20
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[21] ? "extracted" : "")} id="21">
+        <span className={"number " +(table[21] ? "extracted" : "")} id="21">
           21
         </span>
-        <span className={"number " +(table.value[22] ? "extracted" : "")} id="22">
+        <span className={"number " +(table[22] ? "extracted" : "")} id="22">
           22
         </span>
-        <span className={"number " +(table.value[23] ? "extracted" : "")} id="23">
+        <span className={"number " +(table[23] ? "extracted" : "")} id="23">
           23
         </span>
-        <span className={"number " +(table.value[24] ? "extracted" : "")} id="24">
+        <span className={"number " +(table[24] ? "extracted" : "")} id="24">
           24
         </span>
-        <span className={"number " +(table.value[25] ? "extracted" : "")} id="25">
+        <span className={"number " +(table[25] ? "extracted" : "")} id="25">
           25
         </span>
-        <span className={"number " +(table.value[26] ? "extracted" : "")} id="26">
+        <span className={"number " +(table[26] ? "extracted" : "")} id="26">
           26
         </span>
-        <span className={"number " +(table.value[27] ? "extracted" : "")} id="27">
+        <span className={"number " +(table[27] ? "extracted" : "")} id="27">
           27
         </span>
-        <span className={"number " +(table.value[28] ? "extracted" : "")} id="28">
+        <span className={"number " +(table[28] ? "extracted" : "")} id="28">
           28
         </span>
-        <span className={"number " +(table.value[29] ? "extracted" : "")} id="29">
+        <span className={"number " +(table[29] ? "extracted" : "")} id="29">
           29
         </span>
-        <span className={"number " +(table.value[30] ? "extracted" : "")} id="30">
+        <span className={"number " +(table[30] ? "extracted" : "")} id="30">
           30
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[31] ? "extracted" : "")} id="31">
+        <span className={"number " +(table[31] ? "extracted" : "")} id="31">
           31
         </span>
-        <span className={"number " +(table.value[32] ? "extracted" : "")} id="32">
+        <span className={"number " +(table[32] ? "extracted" : "")} id="32">
           32
         </span>
-        <span className={"number " +(table.value[33] ? "extracted" : "")} id="33">
+        <span className={"number " +(table[33] ? "extracted" : "")} id="33">
           33
         </span>
-        <span className={"number " +(table.value[34] ? "extracted" : "")} id="34">
+        <span className={"number " +(table[34] ? "extracted" : "")} id="34">
           34
         </span>
-        <span className={"number " +(table.value[35] ? "extracted" : "")} id="35">
+        <span className={"number " +(table[35] ? "extracted" : "")} id="35">
           35
         </span>
-        <span className={"number " +(table.value[36] ? "extracted" : "")} id="36">
+        <span className={"number " +(table[36] ? "extracted" : "")} id="36">
           36
         </span>
-        <span className={"number " +(table.value[37] ? "extracted" : "")} id="37">
+        <span className={"number " +(table[37] ? "extracted" : "")} id="37">
           37
         </span>
-        <span className={"number " +(table.value[38] ? "extracted" : "")} id="38">
+        <span className={"number " +(table[38] ? "extracted" : "")} id="38">
           38
         </span>
-        <span className={"number " +(table.value[39] ? "extracted" : "")} id="39">
+        <span className={"number " +(table[39] ? "extracted" : "")} id="39">
           39
         </span>
-        <span className={"number " +(table.value[40] ? "extracted" : "")} id="40">
+        <span className={"number " +(table[40] ? "extracted" : "")} id="40">
           40
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[41] ? "extracted" : "")} id="41">
+        <span className={"number " +(table[41] ? "extracted" : "")} id="41">
           41
         </span>
-        <span className={"number " +(table.value[42] ? "extracted" : "")} id="42">
+        <span className={"number " +(table[42] ? "extracted" : "")} id="42">
           42
         </span>
-        <span className={"number " +(table.value[43] ? "extracted" : "")} id="43">
+        <span className={"number " +(table[43] ? "extracted" : "")} id="43">
           43
         </span>
-        <span className={"number " +(table.value[44] ? "extracted" : "")} id="44">
+        <span className={"number " +(table[44] ? "extracted" : "")} id="44">
           44
         </span>
-        <span className={"number " +(table.value[45] ? "extracted" : "")} id="45">
+        <span className={"number " +(table[45] ? "extracted" : "")} id="45">
           45
         </span>
-        <span className={"number " +(table.value[46] ? "extracted" : "")} id="46">
+        <span className={"number " +(table[46] ? "extracted" : "")} id="46">
           46
         </span>
-        <span className={"number " +(table.value[47] ? "extracted" : "")} id="47">
+        <span className={"number " +(table[47] ? "extracted" : "")} id="47">
           47
         </span>
-        <span className={"number " +(table.value[48] ? "extracted" : "")} id="48">
+        <span className={"number " +(table[48] ? "extracted" : "")} id="48">
           48
         </span>
-        <span className={"number " +(table.value[49] ? "extracted" : "")} id="49">
+        <span className={"number " +(table[49] ? "extracted" : "")} id="49">
           49
         </span>
-        <span className={"number " +(table.value[50] ? "extracted" : "")} id="50">
+        <span className={"number " +(table[50] ? "extracted" : "")} id="50">
           50
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="51">
+        <span className={"number " +(table[51] ? "extracted" : "")} id="51">
           51
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="52">
+        <span className={"number " +(table[52] ? "extracted" : "")} id="52">
           52
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="53">
+        <span className={"number " +(table[53] ? "extracted" : "")} id="53">
           53
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="54">
+        <span className={"number " +(table[54] ? "extracted" : "")} id="54">
           54
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="55">
+        <span className={"number " +(table[55] ? "extracted" : "")} id="55">
           55
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="56">
+        <span className={"number " +(table[56] ? "extracted" : "")} id="56">
           56
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="57">
+        <span className={"number " +(table[57] ? "extracted" : "")} id="57">
           57
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="58">
+        <span className={"number " +(table[58] ? "extracted" : "")} id="58">
           58
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="59">
+        <span className={"number " +(table[59] ? "extracted" : "")} id="59">
           59
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="60">
+        <span className={"number " +(table[60] ? "extracted" : "")} id="60">
           60
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="61">
+        <span className={"number " +(table[61] ? "extracted" : "")} id="61">
           61
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="62">
+        <span className={"number " +(table[62] ? "extracted" : "")} id="62">
           62
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="63">
+        <span className={"number " +(table[63] ? "extracted" : "")} id="63">
           63
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="64">
+        <span className={"number " +(table[64] ? "extracted" : "")} id="64">
           64
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="65">
+        <span className={"number " +(table[65] ? "extracted" : "")} id="65">
           65
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="66">
+        <span className={"number " +(table[66] ? "extracted" : "")} id="66">
           66
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="67">
+        <span className={"number " +(table[67] ? "extracted" : "")} id="67">
           67
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="68">
+        <span className={"number " +(table[68] ? "extracted" : "")} id="68">
           68
         </span>
-        <span className="number nice" id="69">
+        <span className={"number nice " +(table[69] ? "extracted" : "")} id="69">
           69 <span className="nice-text">nice!</span>
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="70">
+        <span className={"number " +(table[70] ? "extracted" : "")} id="70">
           70
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="71">
+        <span className={"number " +(table[71] ? "extracted" : "")} id="71">
           71
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="72">
+        <span className={"number " +(table[72] ? "extracted" : "")} id="72">
           72
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="73">
+        <span className={"number " +(table[73] ? "extracted" : "")} id="73">
           73
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="74">
+        <span className={"number " +(table[74] ? "extracted" : "")} id="74">
           74
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="75">
+        <span className={"number " +(table[75] ? "extracted" : "")} id="75">
           75
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="76">
+        <span className={"number " +(table[76] ? "extracted" : "")} id="76">
           76
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="77">
+        <span className={"number " +(table[77] ? "extracted" : "")} id="77">
           77
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="78">
+        <span className={"number " +(table[78] ? "extracted" : "")} id="78">
           78
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="79">
+        <span className={"number " +(table[79] ? "extracted" : "")} id="79">
           79
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="80">
+        <span className={"number " +(table[80] ? "extracted" : "")} id="80">
           80
         </span>
       </div>
       <div className="row">
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="81">
+        <span className={"number " +(table[81] ? "extracted" : "")} id="81">
           81
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="82">
+        <span className={"number " +(table[82] ? "extracted" : "")} id="82">
           82
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="83">
+        <span className={"number " +(table[83] ? "extracted" : "")} id="83">
           83
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="84">
+        <span className={"number " +(table[84] ? "extracted" : "")} id="84">
           84
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="85">
+        <span className={"number " +(table[85] ? "extracted" : "")} id="85">
           85
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="86">
+        <span className={"number " +(table[86] ? "extracted" : "")} id="86">
           86
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="87">
+        <span className={"number " +(table[87] ? "extracted" : "")} id="87">
           87
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="88">
+        <span className={"number " +(table[88] ? "extracted" : "")} id="88">
           88
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="89">
+        <span className={"number " +(table[89] ? "extracted" : "")} id="89">
           89
         </span>
-        <span className={"number " +(table.value[5] ? "extracted" : "")} id="90">
+        <span className={"number " +(table[90] ? "extracted" : "")} id="90">
           90
         </span>
       </div>
-      <span>
-          {length}
-        </span>
     </div>
   );
 }
